4c4ad1f55cf2b3a01380e3125b4339dbfb9f5dfd
Randomshot <sksghgus7175@gmail.com>
2021-05-22 17:21:13 +0900

    Add Exercise RecyclerView

--- TeamUnderDog/app/src/main/java/com/example/teamunderdog/ExerciseListActivity.kt
+++ TeamUnderDog/app/src/main/java/com/example/teamunderdog/ExerciseListActivity.kt
@@ -2,7 +2,9 @@ package com.example.teamunderdog
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.teamunderdog.databinding.ActivityExerciseListBinding
+import java.time.LocalDate
 
 class ExerciseListActivity : AppCompatActivity() {
     lateinit var binding: ActivityExerciseListBinding
@@ -12,8 +14,25 @@ class ExerciseListActivity : AppCompatActivity() {
         setContentView(binding.root)
         init()
     }
+    fun setDataAtFragment(fragment: Fragment, date:String){
+        //kinds : all, wrong ,favorites
+        val bundle = Bundle()
+        bundle.putString("date",date)
+        fragment.arguments = bundle
+        setFragment(fragment)
 
+    }
+    fun setFragment(fragment: Fragment){
+        val transaction =supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.exerciseListFrameLayout,fragment)
+        transaction.commit()
+    }
     private fun init() {
-        TODO("Not yet implemented")
+        var date = LocalDate.now().toString()
+        setDataAtFragment(ExerciseListFragment(),date)
+
+        binding.addExerciseBtn.setOnClickListener {
+
+        }
     }
 }
--- TeamUnderDog/app/src/main/java/com/example/teamunderdog/ExerciseListFragment.kt
+++ TeamUnderDog/app/src/main/java/com/example/teamunderdog/ExerciseListFragment.kt
@@ -5,17 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.teamunderdog.databinding.FragmentExerciseListBinding
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 
 class ExerciseListFragment : Fragment() {
 
+    lateinit var adapter: ExerciseAdapter
+    lateinit var rdb: DatabaseReference
+    private var date:String?= null
     var binding: FragmentExerciseListBinding?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
+        date =arguments?.getString("date")
     }
 
     override fun onCreateView(
@@ -26,5 +31,30 @@ class ExerciseListFragment : Fragment() {
         binding = FragmentExerciseListBinding.inflate(layoutInflater)
         return binding!!.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        val rdbPath = "Exercise/" + date
+        rdb = FirebaseDatabase.getInstance().getReference(rdbPath)
+        binding!!.exerciseListRecyclerView.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL,false)
+        val query = rdb.limitToFirst(100)
+        val option = FirebaseRecyclerOptions.Builder<ExerciseData>()
+            .setQuery(query, ExerciseData::class.java)
+            .build()
+        adapter = ExerciseAdapter(option)
+        adapter.itemClickListener = object:ExerciseAdapter.OnItemClickListener{
+            override fun OnItemClick(view: View, position: Int) {
+
+            }
+        }
+
+        binding!!.exerciseListRecyclerView.adapter = adapter
+        adapter.startListening()
+    }
+
 
 }
