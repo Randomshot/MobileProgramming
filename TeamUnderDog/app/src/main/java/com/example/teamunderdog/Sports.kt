b2fd56ca02e114d306ae1965e517945dc8e46e78
MOLOZISE <wchs0314@gmail.com>
2021-05-23 12:03:54 +0900

    x

---
+++ TeamUnderDog/app/src/main/java/com/example/teamunderdog/Sports.kt
@@ -0,0 +1,6 @@
+package com.e.myfbdb
+
+// id, 이름, 개수(false) or 시간(true), 양(count)
+data class Sports(var sId:Int, var sName:String, var k:Boolean, var sCount:Int) {
+    constructor():this(0, "noinfo", true, 0)
+}
