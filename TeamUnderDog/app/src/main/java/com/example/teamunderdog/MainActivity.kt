54d020ea5413a83ad85f9292988e56d0aa170db8
Randomshot <sksghgus7175@gmail.com>
2021-05-25 14:24:52 +0900

    merge monolosize and randomshot

--- TeamUnderDog/app/src/main/java/com/example/teamunderdog/MainActivity.kt
+++ TeamUnderDog/app/src/main/java/com/example/teamunderdog/MainActivity.kt
@@ -3,16 +3,28 @@ package com.example.teamunderdog
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.teamunderdog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         init()
     }
 
     private fun init() {
-        val intent = Intent(this, ExerciseListActivity::class.java)
-        startActivity(intent)
+
+        binding.button.setOnClickListener {
+
+            val intent = Intent(this, ExerciseListActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.button2.setOnClickListener {
+            val intent = Intent(this, RoutineActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
