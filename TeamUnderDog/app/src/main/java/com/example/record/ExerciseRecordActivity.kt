afe5c611b64b87d52e4f2faebca5c8e669c26bb6
Randomshot <sksghgus7175@gmail.com>
2021-06-19 15:49:11 +0900

    add record

--- TeamUnderDog/app/src/main/java/com/example/record/ExerciseRecordActivity.kt
+++ TeamUnderDog/app/src/main/java/com/example/record/ExerciseRecordActivity.kt
@@ -6,25 +6,27 @@ import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.teamunderdog.databinding.ActivityExerciseRecordBinding
-
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import java.time.LocalDate
 
 class ExerciseRecordActivity : AppCompatActivity() {
     lateinit var binding: ActivityExerciseRecordBinding
     lateinit var layoutManager: LinearLayoutManager
     lateinit var adapter: ExerciseRecordAdapter
     lateinit var rdb: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseRecordBinding.inflate(layoutInflater)
         setContentView(binding.root)
         init()
     }
-
-    private fun init() {
-        val rdbpath = "MyRecord/items"
+    private fun initRecycler(date: String){
+        val rdbpath = "MyRecord/"+date
+        var total = 0
+        var num =0
         rdb = FirebaseDatabase.getInstance().getReference(rdbpath)
         layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         val query = rdb.limitToLast(50)
@@ -32,11 +34,33 @@ class ExerciseRecordActivity : AppCompatActivity() {
                 .setQuery(query, ExerciseRecordData::class.java)
                 .build()
         adapter = ExerciseRecordAdapter(option)
-        binding.apply {
-            recyclerView.layoutManager = layoutManager
-            recyclerView.adapter = adapter
-        }
+
+        for (i : Int in 1..adapter.itemCount){
+            total += adapter.getItem(i-1).eRecord.toInt()
+        }  //이부분이 왜 안될까요???
+        binding.totalRecord.setText("총 운동 시간 : "+(total / 60).toString() + "분 "+(total%60).toString()+"초")
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = adapter
         adapter.startListening()
+    }
+    private fun init() {
+        var date = LocalDate.now().toString()
+
+        initRecycler(date)
+
+        binding.datePicker2.setOnDateChangedListener { view, year, monthOfYear, dayOfMonth ->
+            adapter.stopListening()
+            if(monthOfYear>=9&&dayOfMonth>=9){
+                date = year.toString()+"-"+(monthOfYear+1).toString()+"-"+dayOfMonth.toString()
+            }
+            else if (monthOfYear <10 && dayOfMonth >= 9){
+                date = year.toString()+"-0"+(monthOfYear+1).toString()+"-"+dayOfMonth.toString()
+            }
+            else{
+                 date = year.toString()+"-0"+(monthOfYear+1).toString()+"-0"+dayOfMonth.toString()
+            }
+            initRecycler(date)
+        }
 
         val simpleCallback = object : ItemTouchHelper.SimpleCallback(
                 ItemTouchHelper.DOWN or ItemTouchHelper.UP,
@@ -55,3 +79,4 @@ class ExerciseRecordActivity : AppCompatActivity() {
         itemTouchHelper.attachToRecyclerView(binding.recyclerView)
     }
 }
+
--- TeamUnderDog/app/src/main/java/com/example/record/ExerciseRecordData.kt
+++ TeamUnderDog/app/src/main/java/com/example/record/ExerciseRecordData.kt
@@ -1,5 +1,5 @@
 package com.example.teamunderdog.record
 
-data class ExerciseRecordData(var eId:Int, var eTitle:String, var eRecord:String, var eDate:String) {
-    constructor():this(-1,"","","")
+data class ExerciseRecordData(var eId:Int, var eTitle:String, var eRecord:Int, var eDate:String) {
+    constructor():this(-1,"",0,"")
 }
